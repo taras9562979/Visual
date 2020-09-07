1961d3abe4bab89ded53d339512933e982a21c4b
Taras Kiritchenko <taras.kiritchenko@gmail.com>
2020-09-07 19:28:12 +0300

    first commit

---
+++ graph_visual.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import math
+
+field = tk.Tk()
+field.title('GRAPH VISUALIZATION')
+
+canvas = tk.Canvas(field,width=1600,height=1000,bg='white')
+canvas.pack()
+
+field.mainloop()
