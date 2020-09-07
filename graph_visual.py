aa356b2088b816f2a347c17edfd8c9354edf8f6b
Taras Kiritchenko <taras.kiritchenko@gmail.com>
2020-09-07 21:15:07 +0300

    drawing points and reading their coordinates

--- graph_visual.py
+++ graph_visual.py
@@ -1,10 +1,27 @@
 import tkinter as tk
 import math
 
+nodes = []
+flag_mode = False
+
+def get_left_click(event):
+    global flag_mode
+    if not flag_mode:
+        x = event.x
+        y = event.y
+        radius = 10
+        print('Left Click ->  ',x,y)
+        canvas.create_oval(x-radius,y-radius,x+radius,y+radius,fill='red')
+        nodes.append((x,y))
+    
+        print('Nodes ->  ',nodes)
+
 field = tk.Tk()
 field.title('GRAPH VISUALIZATION')
 
 canvas = tk.Canvas(field,width=1600,height=1000,bg='white')
 canvas.pack()
+
+field.bind('<Button-1>',get_left_click)
 
 field.mainloop()
